573c4cfefa88f01e4cac8f95622c69b2e5120e6c
Adrian Losoya <adrianlosoya791@gmail.com>
2021-05-19 21:50:08 -0700

    create home page

--- app.js
+++ app.js
@@ -20,8 +20,13 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 app.listen(3000, () => {
     console.log('Server is live');
