9d18fd719da982299c7adc18011a7e6d27a9082a
Adrian Losoya <adrianlosoya791@gmail.com>
2021-05-19 21:17:01 -0700

    connect to mongoDB

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/tshirtDB', {
+    useCreateIndex: true, 
+    useFindAndModify: false, 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database Connected');
+});
