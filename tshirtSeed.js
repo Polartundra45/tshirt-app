26b59fe8c289430ee1b5eef75964607ccc62c581
Adrian Losoya <adrianlosoya791@gmail.com>
2021-05-25 20:17:08 -0700

    add product image

--- tshirtSeed.js
+++ tshirtSeed.js
@@ -16,25 +16,25 @@ db.once('open', () => {
 
 const tshirtSeeds = [
     {
-        image: 'abc',
+        image: 'https://images.unsplash.com/photo-1616317038822-55eb03a3bc2f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1834&q=80',
         name: 'rose', 
         price : 15, 
         size : 'sm', 
     },
     {
-        image: 'abc',
+        image: 'https://images.unsplash.com/photo-1621627107348-85f0ecd3f633?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80',
         name: 'mutant', 
         price : 20, 
         size : 'xL', 
     },
     {
-        image: 'abc',
+        image: 'https://images.unsplash.com/photo-1621123073224-c10821c4224e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80',
         name: 'angry', 
         price : 10, 
         size : 'xxL', 
     },
     {
-        image: 'abc',
+        image: 'https://images.unsplash.com/photo-1620935325184-f23de84c5bdf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1875&q=80',
         name: 'dagger', 
         price : 15, 
         size : 'L', 
